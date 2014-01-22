ac807084fe2cd3d844feae5f14eca4827f427220
magdalenka <magdalena.karys@gmail.com>
2014-01-22 11:55:18 +0100

    Historia

--- SMSCollect/About.aspx.cs
+++ SMSCollect/About.aspx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +9,40 @@ public partial class About : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        TableRow row1 = new TableRow();
+        TableCell odbiorca = new TableCell();
+        odbiorca.Text = "DSTD 1CA";
+        TableCell tresc = new TableCell();
+        tresc.Text = "Zajec nie bedzie";
+        TableCell data = new TableCell();
+        data.Text = "22.01.14";
+        TableCell godzina = new TableCell();
+        godzina.Text = "10:15";
+        TableCell dostarczone = new TableCell();
+        dostarczone.Text = "30/30";
+        row1.Cells.Add(odbiorca);
+        row1.Cells.Add(tresc);
+        row1.Cells.Add(data);
+        row1.Cells.Add(godzina);
+        row1.Cells.Add(dostarczone);
+        myTable.Rows.Add(row1);
 
+        TableRow row2 = new TableRow();
+        TableCell odbiorca1 = new TableCell();
+        odbiorca1.Text = "DSTD 1CB";
+        TableCell tresc1 = new TableCell();
+        tresc1.Text = "Spoznie sie 80 minut. Czekajcie";
+        TableCell data1 = new TableCell();
+        data1.Text = "23.01.14";
+        TableCell godzina1 = new TableCell();
+        godzina1.Text = "11:15";
+        TableCell dostarczone1 = new TableCell();
+        dostarczone1.Text = "29/30";
+        row2.Cells.Add(odbiorca1);
+        row2.Cells.Add(tresc1);
+        row2.Cells.Add(data1);
+        row2.Cells.Add(godzina1);
+        row2.Cells.Add(dostarczone1);
+        myTable.Rows.Add(row2);
     }
 }
