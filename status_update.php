753443263aa0f846f627bb3128dbb578078da2ce
Magdalena Karyś <magdalena.karys@gmail.com>
2014-04-28 01:46:25 +0200

    skrypt php do aktualizacji statusow dostarczenia w bazie

---
+++ status_update.php
@@ -0,0 +1,71 @@
+<html>
+<head>
+<title>Online PHP Script Execution</title>
+</head>
+<body>
+<?php
+
+//echo "test";
+
+if($_GET['MsgId']&&$_GET['status'] ) {
+
+$myServer = "mssql.wmi.amu.edu.pl";
+$myUser = "smscollect";
+$myPass = "P6JMrSRu";
+
+//connection to the database
+$dbhandle = mssql_connect($myServer, $myUser, $myPass)
+  or die("Couldn't connect to SQL Server on $myServer"); 
+
+
+$arIds=explode(',',$_GET['MsgId']);
+$arStatus=explode(',',$_GET['status']);
+$arTo=explode(',',$_GET['to']);
+
+if($arIds){
+foreach($arIds as $k => $v ){
+	//$query = "INSERT INTO statusy (sms_status,sms_index,sms_to) VALUES ('".$arStatus[$k]."','".$arIds[$k]."','".$arTo[$k]."')";
+	$query = "UPDATE statusy SET sms_status = '".$arStatus[$k]."', sms_to = '".$arTo[$k]."' WHERE sms_index = '".$arIds[$k]."'";
+	mssql_query($query);
+	if( $arStatus[$k] == "404")
+	{
+		$insert = "INSERT INTO brednie (tekst) VALUES('".$arIds[$k]."')";
+		mssql_query($insert);
+		
+		$select_msg = "SELECT id_mgs FROM statusy WHERE sms_index = '".$arIds[$k]."'";
+		$result = mssql_query($select_msg);
+		//$ile = mssql_num_rows($result)
+		$row = mssql_fetch_row($result);
+		
+		$id_msg1 = $row[0];
+		
+		mssql_free_result($result);
+		//$insert = "INSERT INTO brednie (tekst) VALUES('".$ile."')";
+		//mssql_query($insert);
+		
+		$insert = "INSERT INTO brednie (tekst) VALUES('".$id_msg1."')";
+		mssql_query($insert);
+		
+		$select_count = "SELECT COUNT(*) FROM statusy WHERE id_mgs = '".$id_msg1."'";
+		$result1 = mssql_query($select_count);
+		$count = mssql_num_rows($result1);
+		mssql_free_result($result1);
+		
+		$insert1 = "INSERT INTO brednie (tekst) VALUES('".$count."')";
+		mssql_query($insert1);
+		
+		$update = "UPDATE tresc_sms SET Ilosc_wyslanych = ".$count." WHERE Id_tresc = '".$id_msg1."'";
+		mssql_query($update);
+		
+	}
+	
+}
+
+//close the connection
+mssql_close($dbhandle);
+echo "OK";
+}
+}
+?>
+</body>
+</html>
